c4644bad01059d173510cfb2645fcd339af21b1c
Mikaelpro1010 <mkpro10@alu.ufc.br>
2025-09-14 19:44:24 -0300

    fix: removendo rotas desnecessarias

--- src/routes/livroRoutes.ts
+++ src/routes/livroRoutes.ts
@@ -6,9 +6,6 @@ const router = Router();
 // Rotas CRUD (retornam mensagem informativa)
 router.get('/livros', livroController.getAll);
 router.get('/livros/:id', livroController.getById);
-router.post('/livros', livroController.create);
-router.put('/livros/:id', livroController.update);
-router.delete('/livros/:id', livroController.delete);
 
 // Rota principal de busca
 router.get('/buscar', livroController.buscar);
